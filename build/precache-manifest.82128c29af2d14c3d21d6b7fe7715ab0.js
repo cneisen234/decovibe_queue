bbf1052239b87311aa01bd3162364f52d24f69fc
Christopher Neisen <christopherjay71186@gmail.com>
2020-12-17 12:53:48 -0600

    trying again

---
+++ build/precache-manifest.82128c29af2d14c3d21d6b7fe7715ab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29876dcaa7da89bb6fa784fdd4d29af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8147af064f1baf72cf3b",
+    "url": "/static/css/2.c72968ad.chunk.css"
+  },
+  {
+    "revision": "f7ddc63721b4f90a1242",
+    "url": "/static/css/main.f7bec475.chunk.css"
+  },
+  {
+    "revision": "8147af064f1baf72cf3b",
+    "url": "/static/js/2.f9d548bb.chunk.js"
+  },
+  {
+    "revision": "6e57b14e175cf55b0aae040e5bd49dba",
+    "url": "/static/js/2.f9d548bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ddc63721b4f90a1242",
+    "url": "/static/js/main.77ab75a4.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  }
+]);
